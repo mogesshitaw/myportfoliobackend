24b1c224676b0173884ffe7354ffd7f038489974
mogesshitaw <mogesshitaw4630@gmail.com>
2026-03-23 15:54:17 +0300

    add persima linux

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,5 @@
-import { defineConfig } from '@prisma/config' 
-import { config } from 'dotenv' 
- 
-config() 
- 
-export default defineConfig({ 
-  datasource: { 
-    url: process.env.DATABASE_URL, 
-  }, 
-  schema: './prisma/schema.prisma', 
-  migrations: { 
-    path: './prisma/migrations', 
-  }, 
-}) 
+import { defineConfig } from 'prisma/config'
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+})
