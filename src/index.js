36997695f688963510a373e5ff9b56a3900ccb78
mogesshitaw <mogesshitaw4630@gmail.com>
2026-04-23 09:51:18 +0300

    update 1

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin:  'http://localhost:3000', // process.env.FRONTEND_URL ||
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000', // ||
   credentials: true
 }));
 
